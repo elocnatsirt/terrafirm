9b993cdc6ac2c2837cdb577177ed475695940655
Tristan Cole <tcole@signalpath.com>
2019-02-16 17:22:56 -0500

    Remove secrets and other unnecessary functions. Simplify run_terraform function and ensure it only deletes local state file instead of entire local state directory. Add role function.

--- terrafirm_files/terrafirm_variables.sh
+++ terrafirm_files/terrafirm_variables.sh
@@ -1,24 +1,11 @@
 #!/usr/bin/env bash
 
-#######################################
-########## PROJECT VARIABLES ##########
-#######################################
+#########################################
+########## TERRAFIRM VARIABLES ##########
+#########################################
 
 # Name of your Terraform project
-project_name="terrafirm"
-# S3 Bucket to store Terraform states
-s3_bucket="terraform-states"
-# S3 Bucket region to store Terraform states
-s3_bucket_region="us-east-1"
-# List of valid environments
-my_environments=( dev stage prod )
+project_name="terraform"
 
-
-######################################
-########### USER VARIABLES ###########
-######################################
-
-# AWS Profile to store states
-aws_profile="default"
-# AWS Credentials file
-aws_creds_file="$HOME/.aws/credentials"
+# Init options
+init_opts="-backend-config=bucket=terrafirm-states -backend-config=region=us-east-1 -backend-config=profile=default -backend-config=shared_credentials_file=$HOME/.aws/credentials"
